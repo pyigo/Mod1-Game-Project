774946eac5e99e08ef0b3cebcd6a42427426ff09
Paulette YIGO <paulette@Paulettes-MacBook-Air.local>
2021-12-22 13:46:25 -0500

    modified background image added grid layout

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-// // create game object
-// const game = {
-//     round1: [
-//         question: {
-//             label: ""
-
-//         }
-//     ]
-// }
+// create game object
+// create round objects
+// create questions objects with properties
+const game = {
+    round1: [
+        question1 = {
+            label: "Which of the following is commonly used by a child to build a sandcastle?",
+            responses: ["An architect", "Reinforced steel beams", "Hired labor", "A bucket"],
+            answer: 3
+        },
+        question2 = {
+            label: "What can chameleons change?",
+            responses: ["Hairstyle", "color", "Political party", "Underwear"],
+            answer: 1
+        },
+        question3 = {
+            label: "A magnet would most likley attract which of the following?",
+            responses: ["Metal", "Plastic", "Wood", "The wrong man"],
+            answer: 0
+        }
+    ]
+};
