4c514755deddd37022e3f350289c3ad148170058
Paulette YIGO <paulette@Paulettes-MacBook-Air.local>
2021-12-30 10:22:04 -0500

    modified game page and styling

--- script.js
+++ script.js
@@ -1,28 +1,94 @@
 // create game object
 // create round objects
 // create questions objects with properties
-const game = {
-    round1: [
-        question1 = {
-            label: "Which of the following is commonly used by a child to build a sandcastle?",
-            responses: ["An architect", "Reinforced steel beams", "Hired labor", "A bucket"],
-            answer: 3
-        },
-        question2 = {
-            label: "What can chameleons change?",
-            responses: ["Hairstyle", "color", "Political party", "Underwear"],
-            answer: 1
-        },
-        question3 = {
-            label: "A magnet would most likley attract which of the following?",
-            responses: ["Metal", "Plastic", "Wood", "The wrong man"],
-            answer: 0
-        }
-    ]
+const game = [
+    {
+        questions: [
+            {
+                label:
+                    "Which of the following is commonly used by a child to build a sandcastle?",
+                responses: [
+                    "An architect",
+                    "Reinforced steel beams",
+                    "Hired labor",
+                    "A bucket",
+                ],
+                answer: 3,
+            },
+            {
+                label: "What can chameleons change?",
+                responses: ["Hairstyle", "color", "Political party", "Underwear"],
+                answer: 1,
+            },
+            {
+                label: "A magnet would most likley attract which of the following?",
+                responses: ["Metal", "Plastic", "Wood", "The wrong man"],
+                answer: 0,
+            },
+        ],
+    },
+    {
+        questions: [
+            {
+                label:
+                    "Which of the following is commonly used by a child to build a sandcastle?",
+                responses: [
+                    "An architect",
+                    "Reinforced steel beams",
+                    "Hired labor",
+                    "A bucket",
+                ],
+                answer: 3,
+            },
+            {
+                label: "What can chameleons change?",
+                responses: ["Hairstyle", "color", "Political party", "Underwear"],
+                answer: 1,
+            },
+            {
+                label: "A magnet would most likley attract which of the following?",
+                responses: ["Metal", "Plastic", "Wood", "The wrong man"],
+                answer: 0,
+            },
+        ],
+    },
+];
 
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll(".choice-container"));
+
+var questionObject = {};
+var limit = 3;
+var roundIndex = 0,
+    questionIndex = 0;
+//get question object
+availableQuestion = (roundIndex, questionIndex) => {
+    return game[roundIndex].questions[0];
 };
 
-// function playGame() {
-//     round1.label
-//     console.log(round1);
-// }
+//start game
+startGame = () => {
+    getNewQuestion();
+};
+
+getNewQuestion = (roundIndex, questionIndex) => {
+    questionObject = availableQuestion(roundIndex, questionIndex);
+
+    question.innerText = questionObject.label;
+
+    choices.forEach((choice, index) => {
+        const choiceText = choice.querySelector(".choice-text");
+        choice.setAttribute("index", index);
+        choiceText.innerText = questionObject.responses[index];
+    });
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", (e) => {
+        const selectedChoice = e.target;
+        alert("choice", selectedChoice);
+    });
+});
+
+//fire up the game
+startGame();
